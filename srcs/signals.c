0bc1a693fc3889ab3b5f8bc7cda8741694df6d37
jaguillo <juloo.dsi@gmail.com>
2015-02-23 01:58:37 +0100

    Dont catch ignored signals

--- srcs/signals.c
+++ srcs/signals.c
@@ -6,7 +6,7 @@
 /*   By: jaguillo <jaguillo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/02/20 13:23:11 by jaguillo          #+#    #+#             */
-/*   Updated: 2015/02/22 19:58:33 by jaguillo         ###   ########.fr       */
+/*   Updated: 2015/02/23 00:56:14 by jaguillo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,16 +24,18 @@ static t_env	*env_save(t_env *env)
 	return (save);
 }
 
+static void		sigwinch_handler(int sig)
+{
+	update_term(env_save(NULL));
+	print_list(env_save(NULL));
+	(void)sig;
+}
+
 static void		sig_handler(int sig)
 {
-	if (sig == SIGWINCH)
-	{
-		update_term(env_save(NULL));
-		print_list(env_save(NULL));
-		return ;
-	}
 	restore_term(env_save(NULL));
 	exit(0);
+	(void)sig;
 }
 
 void			listen_signals(t_env *env)
@@ -43,5 +45,10 @@ void			listen_signals(t_env *env)
 	env_save(env);
 	i = -1;
 	while (++i < 32)
-		signal(i, &sig_handler);
+	{
+		if (i != SIGCHLD && i != SIGURG && i != SIGWINCH && i != SIGIO
+			&& i != SIGCONT && i != SIGKILL && i != SIGSTOP)
+			signal(i, &sig_handler);
+	}
+	signal(SIGWINCH, &sigwinch_handler);
 }
